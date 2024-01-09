2715a822494a9475e6782f61cb360c756bb054cb
juju149 <julienmercier149@gmail.com>
2024-01-09 16:47:34 +0100

    add add-site function in views

--- PasswordManager/views.py
+++ PasswordManager/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import SiteForm
 
 # Create your views here.
+@login_required
+def add_site(request):
+    form = SiteForm()
+    if request.method == 'POST':
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            form.save(user=request.user)
+            return redirect('list_sites')
+    return render(request, 'add_site.html', {'form': form})
