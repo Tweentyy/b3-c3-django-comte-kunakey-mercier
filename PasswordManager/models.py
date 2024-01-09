ea3c23cc5d6237c1146feeee145df73950fe995f
juju149 <julienmercier149@gmail.com>
2024-01-09 16:15:19 +0100

    create site model

--- PasswordManager/models.py
+++ PasswordManager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Site(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    
