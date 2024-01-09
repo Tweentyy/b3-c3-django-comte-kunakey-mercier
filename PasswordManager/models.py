f0b476adb61e8b9838264a36e923deb05a6e662f
Tweenty <tweenty.pro@gmail.com>
2024-01-09 16:35:23 +0100

    add: max_length to Site.url

--- PasswordManager/models.py
+++ PasswordManager/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Site(models.Model):
     name = models.CharField(max_length=255)
-    url = models.CharField()
+    url = models.CharField(max_length=1000)
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
