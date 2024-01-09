df759a81d8465a8b9765c0dc7a7653c048ecc140
juju149 <julienmercier149@gmail.com>
2024-01-09 17:32:36 +0100

    add index view

--- PasswordManager/views.py
+++ PasswordManager/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import SiteForm
 
-# Create your views here.
-@login_required
+def index(request):
+    return render(request, 'index.html')
+
 def add_site(request):
     form = SiteForm()
     if request.method == 'POST':
         form = SiteForm(request.POST)
         if form.is_valid():
-            form.save(user=request.user)
-            return redirect('list_sites')
-    return render(request, 'add_site.html', {'form': form})
+            form.save()
+    return render(request, 'index.html', {'form': form})
+    
