e1dd851e04c6f8af6dcd3846b4e8be0756c53d10
juju149 <julienmercier149@gmail.com>
2024-01-09 16:17:20 +0100

    update url max_lenght

--- PasswordManager/models.py
+++ PasswordManager/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Site(models.Model):
     name = models.CharField(max_length=255)
-    url = models.CharField(max_length=255)
+    url = models.CharField()
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    
